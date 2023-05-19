4d24b2e47a5a097b43277c0f4b100bdd66e7a9fa
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-05-19 07:35:12 -0300

    Removed User.List

--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,6 @@ namespace learning_dotnet_full_webapi.Models
 {
     public class User
     {
-        static public List<User> List = new List<User>();
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
